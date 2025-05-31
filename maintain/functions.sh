772f1d3c9483e897f0866c97f9d0965345c58941
Pavel Perminov <pavel.perminov.23@gmail.com>
2025-05-31 15:15:12 +0300

    1.docker-entrypoint.sh: almost fully replaced with mysql_entrypoint() call 2.maintain/functions.sh  - re-implemented import_dump()  - added mysql_entrypoint()    - each time mysql server has been shutdown mysql_entrypoint() is called again if /var/lib/mysql is empty      so mysql full re-init achieved just by shutting down and removing mysql data 3.restore: refactored to fully work within wrapper-container

--- maintain/functions.sh
+++ maintain/functions.sh
@@ -619,13 +619,13 @@ restore_dump() {
   # Download database dump from github
   download_dump $release
   
-  # Extract dump from gz-file and import into mysql
+  # Empty mysql data-dir and restart mysql to re-init using downloaded dump
   import_dump
 }
 
 # Download database dump from github
 download_dump() {
-  
+
   # Arguments
   release=$1
 
@@ -637,15 +637,56 @@ download_dump() {
   echo "$msg Done"
 }
 
-# Extract dump from gz-file and import into mysql
+# Shutdown mysql, empty data-dir and wait for mysql to re-init using pre-downloaded dump
 import_dump() {
 
-  # Extract and import
+  # Shut down mysql
   export MYSQL_PWD=$MYSQL_PASSWORD
-  echo -n "Importing $MYSQL_DUMP into MySQL..."
-  zcat data/$MYSQL_DUMP | mysql -h mysql -u $MYSQL_USER $MYSQL_DATABASE
-  echo -e " Done\n"
+  local msg="Shutting down MySQL server..." && echo "$msg"
+  mysql -h mysql -u root -e "SHUTDOWN"
   unset MYSQL_PWD
+
+  # Wait until shutdown is really completed
+  local timeout=60
+  local elapsed=0
+  local done="/var/lib/mysql/shutdown.done"
+  while :; do
+    clear_last_lines 1
+    echo "$msg waiting for completion ($elapsed s)"
+    sleep 1
+    elapsed=$((elapsed + 1))
+    if [ -f "$done" ] || [ $elapsed -ge $timeout ]; then break; fi
+  done
+
+  # If shutdown file was created by mysql-container custom-entrypoint.sh script
+  if [ -f "$done" ]; then
+
+    # It means mysqld process exited gracefully, i.e. shutdown is really completed
+    clear_last_lines 1
+    echo "$msg Done"
+
+    # Empty mysql_server_data volume
+    echo -n "Removing all data from MySQL server..." && rm -rf /var/lib/mysql/* && echo -e " Done"
+
+    # Wait until re-init is really completed
+    local msg="Starting MySQL server with import from data/ dir..." && echo "$msg"
+    local elapsed=0
+    local done="/var/lib/mysql/init.done"
+    while :; do
+      clear_last_lines 1
+      echo "$msg ($elapsed s)"
+      sleep 1
+      elapsed=$((elapsed + 1))
+      if [ -f "$done" ] || [ $elapsed -ge $timeout ]; then break; fi
+    done
+    clear_last_lines 1
+    echo "$msg Done"
+
+  # Else if shutdown is stuck somewhere - print error message and exit
+  else
+    echo "MySQL server shutdown timeout reached, something went wrong :("
+    exit 1
+  fi
 }
 
 # Restore state of custom/data/upload dir from the uploads.zip of a given release tag
@@ -1361,4 +1402,189 @@ backup_before_restore() {
 
   # Print new line
   echo ""
+}
+
+mysql_entrypoint() {
+
+  # Path to a file to be created once init is done
+  done=/var/lib/mysql/init.done
+
+  # If init is not done
+  if [[ ! -f "$done" ]]; then
+
+    # Install certain tools
+    apt-get update && apt-get install -y wget curl jq
+
+    # Print which dump is going to be imported
+    echo "MYSQL_DUMP is '$MYSQL_DUMP'";
+
+    # Change dir
+    cd /docker-entrypoint-initdb.d
+
+    # Array of other sql files to be imported
+    declare -a import=("maxwell.sql")
+
+    # Split MYSQL_DUMP on whitespace into an array
+    IFS=' ' read -ra dumpA <<< "$MYSQL_DUMP"
+
+    # Empty dumps counter
+    empty=0
+
+    # Missing dumps counter
+    missing=0
+
+    # Downloaded/copied dumps counter, to be used in filename prefix to preserve import order
+    prefix=0
+
+    # Foreach dump
+    for dump in "${dumpA[@]}"; do
+
+      # If dump is an URL
+      if [[ $dump == http* ]]; then
+
+        # Extract the filename from the URL and add counter-prefix
+        name="$prefix-$(basename "$dump")"
+
+        # Download it right here
+        echo "Fetching remote MySQL dump from '$dump' into local '$name' ..." && wget --no-check-certificate -O "$name" "$dump"
+
+        # Increment saved dumps counter
+        prefix=$((prefix + 1))
+
+      # Else assume it's a local path pointing to some file inside
+      # /docker-entrypoint-initdb.d/custom/ directory mapped as a volume
+      # from data/ directory on the docker host machine
+      else
+
+        # Shortcut
+        local="custom/$dump"
+
+        # If that local dump file does NOT really exists in data/ directory on host machine
+        # e.g does NOT exist in custom/ directory in mysql-container due to bind volume mapping
+        if [[ ! -f "$local" ]]; then
+
+          # If GH_TOKEN_CUSTOM variable is set - install GitHub CLI
+          if [[ ! -z "$GH_TOKEN_CUSTOM" ]]; then
+            ghcli_install
+            export GH_TOKEN="$GH_TOKEN_CUSTOM"
+          fi
+
+          # Load list of available releases for current repo. If no releases - load ones for parent repo, if current repo
+          # was forked or generated. But anyway, $init_repo and $init_release variables will be set up to clarify where to
+          # download asset from, and it will refer to either current repo, or parent repo for cases when current repo
+          # has no releases so far, which might be true if it's a very first time of the instance getting up and running
+          # for the current repo
+          if (( ${#releaseQty[@]} == 0 )); then
+            load_releases "$(get_current_repo ".gitconfig")" "init"
+          fi
+
+          # Download it from github into data/ dir
+          if [[ ! -z "${init_repo:-}" ]]; then
+            echo "Asset '$dump' will be downloaded from '$init_repo:$init_release'"
+            gh_download "$init_repo" "$init_release" "$dump" "custom"
+          fi
+        fi
+
+        # If that local dump file really exists in data/ directory on host machine
+        # e.g exists  in custom/ directory in mysql-container due to bind volume mapping
+        if [ -f "$local" ]; then
+
+          # If that local dump file is not empty
+          if [ -s "$local" ]; then
+
+            # Copy that file here for it to be imported, as files from subdirectories are ignored while import
+            cp "$local" "$prefix-$dump" && echo "File '/docker-entrypoint-initdb.d/$local' copied to the level up"
+
+            # Increment saved dumps counter
+            prefix=$((prefix + 1))
+
+          # Else
+          else
+
+            # Print warning
+            echo "[Warning] Local SQL dump file '$dump' is empty"
+
+            # Increment empty dumps counter
+            empty=$((empty + 1))
+          fi
+
+        # Else if that local dump file does not really exist
+        else
+
+          # Increment missing dumps counter
+          missing=$((missing + 1))
+        fi
+      fi
+    done
+
+    # If no dump(s) were given by MYSQL_DUMP-variable or all are empty/missing
+    if [ $(( ${#dumpA[@]} - empty - missing )) -eq 0 ]; then
+
+      # Print info
+      echo "None of SQL dump file(s) not found, assuming blank Indi Engine instance setup"
+
+      # Use system.sql
+      import+=("system.sql.gz")
+    fi
+
+    # Feed system token to GitHub CLI
+    GH_TOKEN="$GH_TOKEN_SYSTEM"
+
+    # Foreach file to be imported in addition to file(s) in MYSQL_DUMP
+    for filename in "${import[@]}"; do
+
+      # Relative path of the downloaded file
+      local="custom/$filename"
+
+      # If file does not exist - download it
+      if [ ! -f "$local" ]; then
+        gh_download "indi-engine/system" "default" "${filename}" "custom"
+      fi
+
+      # Copy that file here for it to be imported, as files from subdirectories are ignored while import
+      cp "$local" "$filename" && echo "File '/docker-entrypoint-initdb.d/$local' copied to the level up"
+    done
+
+    # Spoof mysql user name inside maxwell.sql, if need
+    [[ $MYSQL_USER != "custom" ]] && sed -i "s~custom~$MYSQL_USER~" maxwell.sql
+
+    # Сopy 'mysql' and 'mysqldump' command-line utilities into it, so that we can share
+    # those two binaries with apache-container to be able to export/import sql-files
+    src="/usr/bin"
+    vmd="/usr/bin/volumed"
+    cp "$src/mysql"     "$vmd/mysql"
+    cp "$src/mysqldump" "$vmd/mysqldump"
+
+    # Append touch-command to create an empty '/var/lib/mysql/init.done'-file after init is done to use in healthcheck
+    sed -i 's~Ready for start up."~&\n\t\t\ttouch /var/lib/mysql/init.done~' /usr/local/bin/docker-entrypoint.sh
+  fi
+
+  # Call the original entrypoint script
+  /usr/local/bin/docker-entrypoint.sh "$@"
+
+  # If we reached this line, it means mysql was shut down
+  echo "MySQL Server has been shut down"
+
+  # Create a file within data-dir to indicate shutdown is completed
+  touch /var/lib/mysql/shutdown.done
+
+  # Wait until shutdown is really completed
+  local timeout=5
+  local elapsed=0
+  local data="/var/lib/mysql"
+  while [ ! -z "$(ls -A $data)" ] && [ $elapsed -lt $timeout ]; do
+    echo "Waiting for data-directory to be emptied... ($elapsed s)"
+    sleep 1
+    elapsed=$((elapsed + 1))
+  done
+
+  # If data-directory was emptied
+  if [ -z "$(ls -A $data)" ]; then
+
+    # We assume it was done for restore
+    echo "MySQL data-directory has been emptied, so initiating the restore..."
+
+    # Re-init, assuming sql dump file(s) are present in /docker-entrypoint-initdb.d/custom/
+    mysql_entrypoint "$@"
+  fi
 }
